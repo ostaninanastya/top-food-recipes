e16f529e2346681e8fa39930c67f71705d48006d
Nastya <ostanina.anastasia2014@yandex.ru>
2017-10-29 01:43:36 +0300

    spring boot skeleton

---
+++ src/main/java/com/topfood/top_food_recipes/TopFoodRecipesApplication.java
@@ -0,0 +1,12 @@
+package com.topfood.top_food_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopFoodRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopFoodRecipesApplication.class, args);
+	}
+}
