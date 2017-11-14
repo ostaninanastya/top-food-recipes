9fe8567b349a49138edc6b07dc17a1dffc2850ff
slayjoker95 <slayjoker95@gmail.com>
2017-11-14 18:13:03 +0300

    prop

--- src/main/resources/static/js/app/recipeService.js
+++ src/main/resources/static/js/app/recipeService.js
@@ -1,7 +1,7 @@
 angular.module('topFoodRecipesApp')
 .service('RecipeService', function($http) {
     this.getRecipes = function() {
-        return $http.get('http://localhost:8080/top_food_recipes-0.0.1-SNAPSHOT/api/recipe').then(function(response){
+        return $http.get('http://188.166.30.145:8080/top_food_recipes-0.0.1-SNAPSHOT/api/recipe').then(function(response){
             return response.data;
     }).catch(function(err) {
             return [];
@@ -10,7 +10,7 @@ angular.module('topFoodRecipesApp')
 })
 .service('CuisineService', function($http) {
     this.getCuisines = function() {
-        return $http.get('http://localhost:8080/top_food_recipes-0.0.1-SNAPSHOT/api/cuisine').then(function(response){
+        return $http.get('http://188.166.30.145:8080/top_food_recipes-0.0.1-SNAPSHOT/api/cuisine').then(function(response){
             return response.data;
     }).catch(function(err) {
             return [];
@@ -19,7 +19,7 @@ angular.module('topFoodRecipesApp')
 })
     .service('IngredientService', function($http) {
         this.getIngredients = function() {
-            return $http.get('http://localhost:8080/top_food_recipes-0.0.1-SNAPSHOT/api/ingredient').then(function(response){
+            return $http.get('http://188.166.30.145:8080/top_food_recipes-0.0.1-SNAPSHOT/api/ingredient').then(function(response){
                 return response.data;
             }).catch(function(err) {
                 return [];
