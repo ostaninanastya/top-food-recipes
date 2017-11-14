9ec0d9d91735fa127f97b436feabb5f7c8668653
eziyangulov <eduard.ziyangulov@netcracker.com>
2017-11-14 11:15:29 +0300

    A small fix

--- src/main/resources/static/js/app/recipeService.js
+++ src/main/resources/static/js/app/recipeService.js
@@ -1,6 +1,6 @@
 angular.module('topFoodRecipesApp').service('RecipeService', function($http) {
     this.getRecipes = function() {
-        return $http.get('http://localhost:8080/api/recipe').then(function(response){
+        return $http.get('http://localhost:8080/top_food_recipes-0.0.1-SNAPSHOT/api/recipe').then(function(response){
             return response.data;
     }).catch(function(err) {
             return [];
