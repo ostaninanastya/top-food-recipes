a1b17716ab929d14050c8c426031caa08772c734
slayjoker95 <slayjoker95@gmail.com>
2017-11-23 14:29:04 +0300

    add demo_login

---
+++ src/main/resources/static/js/app/controllers.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var controllerModule = angular.module('topFoodRecipeAppControllers', []);
+
+controllerModule.controller('RecipeController', function($scope, $http, RecipeService) {
+    $scope.recipes = [];
+    RecipeService.getRecipes().then(function(recipes){
+        $scope.recipes = recipes;
+    });
+
+});
+
+controllerModule.controller('CuisineController', function($scope, $http, CuisineService) {
+    $scope.cuisines = [];
+    CuisineService.getCuisines().then(function(cuisines){
+        $scope.cuisines = cuisines;
+    });
+
+    $scope.cuisine ={};
+    $scope.submit = function() {
+        CuisineService.addNewCuisine($scope.cuisine).then(function () {
+            CuisineService.getCuisines().then(function(cuisines){
+                $scope.cuisines = cuisines;
+            });
+            console.log("completed");
+        }) ;
+    }
+});
+
+controllerModule.controller('MainController', function($scope, $rootScope, $location, LoginService) {
+    $rootScope.menu = LoginService.getMenu();
+
+    $rootScope.$on('registration_event', function(event, data) {
+        console.log('XXXXX: received data: ' + data);
+        LoginService.loginUser(data, function(response) {
+            if (response.success) {
+                console.log('Successfully logged in under ' + $rootScope.user.name);
+                $rootScope.$broadcast('reload_page_event');
+            } else {
+                console.log('Login under ' + $scope.user.name + ' failed');
+            }
+        });
+    });
+
+    $rootScope.$on('reload_page_event', function(event, data) {
+        $rootScope.menu = LoginService.getMenu();
+        $location.path('/');
+    });
+
+});
+
+
+controllerModule.controller('LoginController', function($scope, $rootScope, $location, LoginService) {
+
+    $scope.login = function() {
+        LoginService.loginUser($scope.user, function(response) {
+            if (response.success) {
+                console.log('Successfully logged in under ' + $rootScope.user.name);
+                $rootScope.$broadcast('reload_page_event');
+            } else {
+                console.log('Login under ' + $scope.user.name + ' failed');
+            }
+        });
+    }
+
+    $scope.$on('registration_event', function(event, data) {
+       console.log('YYYYYY');
+    });
+
+});
+
+controllerModule.controller('RegisterController', function($scope, $rootScope, $location, LoginService) {
+
+    $scope.register = function() {
+        console.log('Register user, name=' + $scope.user.name + ', password=' + $scope.user.password);
+
+        LoginService.registerUser($scope.user, function(response) {
+            if (response.success) {
+                console.log('Success');
+                $rootScope.$broadcast('registration_event', $scope.user);
+            } else {
+                console.log('Fail');
+            }
+        }); 
+
+    }
+});
+
+controllerModule.controller('LogoutController', function($scope, $rootScope, $location, LoginService) {
+
+    $scope.$on('$routeChangeSuccess', function () {
+        $rootScope.user = undefined;
+        $rootScope.$broadcast('reload_page_event');
+    });
+
+});
+
---
+++ src/main/resources/static/js/app/services.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var servicesModule = angular.module('topFoodRecipeAppServices', []);
+
+servicesModule.service('RecipeService', function($http,SERVER_URL) {
+    this.getRecipes = function() {
+        return $http.get(SERVER_URL+'api/recipe').then(function(response){
+            return response.data;
+        }).catch(function(err) {
+            return [{"name" : "рецепт пиццы", "recipe" : "dhduhdishdisj"}];
+        });
+    }
+});
+    
+servicesModule.service('CuisineService', function($http, SERVER_URL) {
+    this.getCuisines = function() {
+        return $http.get(SERVER_URL+'api/cuisine').then(function(response){
+            return response.data;
+        }).catch(function(err) {
+            return [];
+        });
+    }
+        
+    this.addNewCuisine = function(cuisine) {
+        console.log("posting data....");
+        return $http.post(SERVER_URL+'api/cuisine', JSON.stringify(cuisine)).success(function(){
+            console.log("success");
+        });
+    }
+});
+
+servicesModule.service('LoginService', function($http, $rootScope, SERVER_URL) {
+    var allUsers = [];
+
+    this.loginUser = function(user, callback) {
+        console.log('Login user, name=' + user.name + ', password=' + user.password);
+        var resp = { success: false, message: 'Username or password is incorrect' };
+
+        $http.get(SERVER_URL + 'api/users').then(function(response) {
+            allUsers = response.data;
+            console.log('Found ' + allUsers.length + ' users');
+
+            for (var i in allUsers) {
+                var usr = allUsers[i];
+                console.log('Name: ' + usr.name + ', password: ' + usr.password);
+
+                if (usr.name == user.name && usr.password == user.password) {
+                    console.log("User matches!!!");
+                    $rootScope.user = user;
+                    resp = { success: true };
+                }
+            }
+    
+            callback(resp);
+        }).catch(function(err) {
+            console.log('Error occurred: ' + err.statusText);
+            callback(resp);
+        });
+        
+    }
+
+    this.getMenu = function() {
+        var menu = [
+            {
+                title: 'Recipes',
+                url: '#/recipes',
+                f: 'a()'
+            },
+            {
+                title: 'Cuisines',
+                url: '#/cuisines',
+                f: 'a()'
+            },
+            {
+                title: 'Ingredients',
+                url: '#/ingredients',
+                f: 'a()'
+            },
+            {
+                title: 'About',
+                url: '#/about',
+                f: 'a()'
+            }
+        ];
+
+        if ($rootScope.user != undefined) {
+            menu.push({title: 'Logout', url: '#/logout', f: 'logout()'});
+        } else {
+            menu.push({title: 'Login', url: '#/login', f: ''});
+        }
+
+       return menu;
+    }
+
+
+    this.registerUser = function(user, callback) {
+        console.log('Register user, name=' + user.name + ', password=' + user.password);
+        var resp = { success: false, message: 'Failed to register a user' };
+
+        $http.post(SERVER_URL + 'api/users', JSON.stringify(user)).then(function (response) {
+            console.log('Successfully registered user ' + user.name);
+            resp = { success: true };
+            callback(resp);
+        }).catch(function (err) {
+            console.log("Failed to register user " + user.name + ": " + err.statusText);
+            callback(resp);
+        });
+    }
+
+
+
+});
+
