bbc2ad896301db5f6d659263bf70c3b3ad6577aa
slayjoker95 <slayjoker95@gmail.com>
2017-11-30 16:52:55 +0300

    hash

--- src/main/java/com/topfood/recipes/common/enums/ErrorCodeMap.java
+++ src/main/java/com/topfood/recipes/common/enums/ErrorCodeMap.java
@@ -1,5 +1,6 @@
 package com.topfood.recipes.common.enums;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.topfood.recipes.common.enums.ErrorCodes.OK;
@@ -9,6 +10,7 @@ import static com.topfood.recipes.common.enums.ErrorCodes.TOO_OFTEN_LIKES;
 public class ErrorCodeMap {
     public static Map<ErrorCodes, String> errors;
     static {
+        errors = new HashMap<>();
     errors.put(REG_USER_ALREADY_EXISTS, "User already exists");
     errors.put(OK, "");
     errors.put(TOO_OFTEN_LIKES, "User can't do more than 1 like in 2 min");
