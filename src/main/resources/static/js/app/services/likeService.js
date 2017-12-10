0762b6008a34fa2b6082aa0a8f61e8c1fccd8fe4
Edward <edwardz10@github.com>
2017-12-10 18:33:28 +0300

    Got rid of the "reloadRecipesPeriodically" interval function

--- src/main/resources/static/js/app/services/likeService.js
+++ src/main/resources/static/js/app/services/likeService.js
@@ -1,23 +1,38 @@
 servicesModule.service('LikeService', function($http, $rootScope, SERVER_URL) {
 
-    this.addLike = function(recipe, like) {
+    this.addLike = function(recipe, like, callback) {
+        var resp = {success: true};
         like.user = $rootScope.user;
         like.recipe = recipe;
         like.sign = true;
         like.timeStamp = Date.now();
-        return $http.post(SERVER_URL + 'api/like', JSON.stringify(like)).success(function(){
-            console.log("success");
+        
+        $http.post(SERVER_URL + 'api/like', JSON.stringify(like)).then(function (response) {
+            console.log('Successfully put a like ');
+            callback(resp);
+        }).catch(function (response) {
+            console.log('Failed to put a like ');
+            resp = {success: false, errorMessage: 'Failed to put a like: ' + response.data};
+            callback(resp);
         });
+
     }
 
-    this.addDislike = function(recipe, like) {
+    this.addDislike = function(recipe, like, callback) {
+        var resp = {success: true};
         like.user = $rootScope.user;
         like.recipe = recipe;
         like.sign = false;
         like.timeStamp = Date.now();
-        return $http.post(SERVER_URL + 'api/like', JSON.stringify(like)).success(function(){
-            console.log("success");
+
+        $http.post(SERVER_URL + 'api/like', JSON.stringify(like)).then(function (response) {
+            console.log('Successfully put a dislike ');
+            callback(resp);
+        }).catch(function (response) {
+            console.log('Failed to put a dislike ');
+            resp = {success: false, errorMessage: 'Failed to put a dislike: ' + response.data};
+            callback(resp);
         });
     }
 
-    });
+});
