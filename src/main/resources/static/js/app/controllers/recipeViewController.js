4ae2d08f8e8d31afa88337c4bd5f2fd1bb7cdc0d
eziyangulov <eduard.ziyangulov@netcracker.com>
2017-12-12 13:41:36 +0300

    fixed recipes deletion

--- src/main/resources/static/js/app/controllers/recipeViewController.js
+++ src/main/resources/static/js/app/controllers/recipeViewController.js
@@ -1,2 +1,10 @@
-controllerModule.controller('RecipeViewController', function($scope, $http, $rootScope, RecipeService) {
+controllerModule.controller('RecipeViewController', function($scope, $http, $location, RecipeService) {
+
+    $scope.Delete = function () {
+        RecipeService.deleteRecipe($scope.selectedRecipe, function() {
+            RecipeService.getRecipes().then(function(recipes){
+                $location.path('/recipes');
+            });
+        });
+    }
 });
