eedb66f942efd93dd892b13f2ee8524f5010f676
Nastya <ostanina.anastasia2014@yandex.ru>
2017-12-16 16:20:03 +0300

    TOPFOODRECIPES-45

--- src/main/resources/static/js/app/controllers/recipeViewController.js
+++ src/main/resources/static/js/app/controllers/recipeViewController.js
@@ -1,5 +1,18 @@
 controllerModule.controller('RecipeViewController', function($scope, $http, $location, $rootScope, RecipeService) {
 
+    $scope.ingredientRecipeArray = [];
+    RecipeService.getIngredientRecipes().then(function (ingredientsRecipes) {
+        $scope.ingredientsRecipes = ingredientsRecipes;
+        for (var i = 0; i < $scope.ingredientsRecipes.length; i++)
+        {
+            if ($scope.ingredientsRecipes[i].recipe.recipe_id === $rootScope.selectedRecipe.recipe_id)
+            {
+                $scope.ingredientRecipeArray.push($scope.ingredientsRecipes[i]);
+            }
+        }
+    })
+
+
     $scope.Delete = function () {
         RecipeService.deleteRecipe($scope.selectedRecipe, function() {
             RecipeService.getRecipes().then(function(recipes){
@@ -13,4 +26,6 @@ controllerModule.controller('RecipeViewController', function($scope, $http, $loc
         else
             return false;
     }
+
+
 });
